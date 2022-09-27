cb30f1943f40788d3b7b8978ebfb2728d9bbec9a
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2022-09-26 19:44:04 -0700

    file manipulation through node.js and more server code

--- dev/numberOfTheDay/app.js
+++ dev/numberOfTheDay/app.js
@@ -11,14 +11,23 @@
 //to include a module use require()
 const http = require('http');
 const fs = require('fs');
+const uc = require('upper-case');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+function genLargeNum(){
+	return Math.random().toString();
+}
 
 const server = http.createServer((req, res) => {
 	fs.readFile('index.html', ((err, data) => {
 		res.writeHead(200, {'Content-type': 'text/html'});
 		res.write(data);
+		res.write(uc.upperCase("uppercase is a module for lazy turds"));
+		res.write("<br/>");
+		res.write("<br/>");
+		res.write(genLargeNum());
 		return res.end();
 	}));
 });
