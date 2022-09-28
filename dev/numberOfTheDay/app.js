c05c15de297cfd3e3ef18b76fcff6960cbd4b797
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2022-09-27 17:23:26 -0700

    using pure node.js to get forms without express

--- dev/numberOfTheDay/app.js
+++ dev/numberOfTheDay/app.js
@@ -1,5 +1,3 @@
-
-
 //use ctrl+C to cancell and end the server
 
 /*
@@ -7,20 +5,6 @@
  * A process that is blocked is one that is waiting for some event, 
  * such as a resource becoming available or the completion of an I/O operation.[1]
 */
-
-//to include a module use require()
-const http = require('http');
-const fs = require('fs');
-const uc = require('upper-case');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-function genLargeNum(){
-	return Math.floor(Math.random()*(1000000000000-0) + 0).toString();
-}
-
-
 
 /*
  * guess a number.
@@ -30,7 +14,50 @@ function genLargeNum(){
  *
 */
 
+const http = require('http');
+const fs = require('fs');
+const uc = require('upper-case');
+const querystring = ('querystring');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+function genLargeNum(){
+	return Math.floor(Math.random()*(1000000000000-0) + 0).toString();
+}
+
+function parsePost(request, response, callback){
+	var queryData = "";
+	if(typeof callback !== 'function') return null;
+
+	if(request.method == 'POST'){
+		request.on('data', function(data) {
+			queryData+=data;
+			if(queryData.length > 1e6){
+				queryData = "";
+				response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+				request.connection.destroy();
+			}
+		});
+		
+		request.on('end', function() {
+			request.post = querystring.parse(queryData);
+			callback();
+		});
+	}else {
+		response.writeHead(405, {'Content-Type': 'text/plain'});
+		response.end();
+	}
+}
+
+
 const server = http.createServer((req, res) => {
+	if(req.method == 'POST'){
+		parsePost(req, res, function(){
+			console.log("THE RESULTS");
+			console.log(req.post);
+		});
+	}
 	fs.readFile('index.html', ((err, data) => {
 		res.writeHead(200, {'Content-type': 'text/html'});
 		res.write(data);
