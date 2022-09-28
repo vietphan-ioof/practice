332d4cd390e29548d4a6c38b068b89f8e3d90995
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2022-09-28 16:32:25 -0700

    init function and TODO have to create more security

--- dev/numberOfTheDay/app.js
+++ dev/numberOfTheDay/app.js
@@ -23,13 +23,24 @@ const hostname = '127.0.0.1';
 const port = 3000;
 var numOfDay = 0;
 
+
 function genLargeNum(){
 	return Math.floor(Math.random()*(1000000000000-0) + 0).toString();
 }
 
-const server = http.createServer((req, res) => {
-	var RESULT = false;
+function testNumberGenerator(){
+	return Math.floor(Math.random()*(3-0) + 0).toString();
+}
 
+function init(){
+	numOfDay = testNumberGenerator();
+	console.log("NUMER OF THE DAYYYYYYY" + " " + numOfDay);
+}
+
+const server = http.createServer((req, res) => {
+	init();
+	var RESULT = false;
+	
 	if(req.method == "POST"){
 		var body = '';
 
@@ -46,7 +57,6 @@ const server = http.createServer((req, res) => {
 			answer = post['guess'];
 
 			if(numOfDay.toString() === answer){
-
 				RESULT = true;
 			}
 		});
