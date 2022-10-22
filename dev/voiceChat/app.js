874ea3d7d5c292da285dfd66c5f8e97d41510790
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2022-10-21 17:54:37 -0700

    voice call research

--- dev/voiceChat/app.js
+++ dev/voiceChat/app.js
@@ -1,3 +1,16 @@
+/*
+	p2p voice call application
+
+	approaches:
+		p2p:
+			- avast.com/c-p2p-vpn-server#
+		-- webRTC 
+		-- p2p-sip 
+		   - https://en.wikipedia.org/wiki/Peer-to-peer_SIP
+		   - https://github.com/theintencity/sip-js
+*/
+
+
 
 const http = require('http');
 const express = require('express');
