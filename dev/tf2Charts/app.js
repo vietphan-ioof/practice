222ac3d0b6bc5c88f6d158cb605e95181b73e3ca
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2022-10-30 16:17:34 -0700

    custom steam profile finder

--- dev/tf2Charts/app.js
+++ dev/tf2Charts/app.js
@@ -4,14 +4,12 @@
 	https://www.npmjs.com/package/steamapi
 	https://wiki.teamfortress.com/wiki/WebAPI
 	F191259D56059F8FEE6A458710180A24
-
 */
 
 const http = require('http');
 const express = require('express');
 const steamAPI = require('steamapi');
 const path = require('path');
-
 
 //starting the steam api
 const SteamAPI = require('steamapi');
@@ -30,15 +28,6 @@ const port = 3000;
 
 let x = {};
 
-//override console.log behavior
-/*
-console.LOG = console.log;
-
-console.log = function(value){
-	console.LOG(value);
-	window.$log = value;
-}
-*/
 let v = 0;
 
 async function fetchUserData(req, res){
@@ -59,15 +48,27 @@ async function fetchUserData(req, res){
 		});
 }
 
-app.get('/', function(req, res){
+/*
+	set custom steam url
+	set acheivemnts to public
+		 - send exception if not satisfied
+*/
 
-	var fetchId = steam.resolve('https://steamcommunity.com/id/DimGG').then(function(id) {
+app.get('/', function(req, res){
+	let string = 'https://steamcommunity.com/profiles/76561198253369292';
+	let string2 = 'https://steamcommunity.com/id/Dragonsofdra';
+	let string3 = 'https://steamcommunity.com/id/DimGG';
+	let string4 = 'https://steamcommunity.com/id/zkae';
+
+	var fetchId = steam.resolve(string2).then(function(id) {
+		console.log(id);
 		result = steam.getUserStats(id, 440).then(stats => {
+		console.log(stats);
 			let temp = stats.stats;
 			let keys = Object.keys(temp);
 			let result = stats.stats[keys[0]]
 			console.log(result);
-			res.render(path.join(__dirname, './public/index.html'), {RESULT:stats.stats[keys[-2]]});
+			res.render(path.join(__dirname, './public/index.html'), {RESULT:stats.stats[keys[0]]});
 			});
 		});
 });
